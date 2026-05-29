9cd437d66882efa091cdc3113c8e7405013edbec
ahmertsengol <118382440+ahmertsengol@users.noreply.github.com>
2026-05-30 00:16:22 +0300

    Fix mobile directions link

--- src/lib/business.ts
+++ src/lib/business.ts
@@ -24,7 +24,7 @@ export const business = {
   instagramUrl: "https://www.instagram.com/riva.veterinerklinigi",
   instagramHandle: "@riva.veterinerklinigi",
   mapsUrl:
-    "https://www.google.com/maps/dir/?api=1&destination=Riva+Veteriner+Kliniği&destination_place_id=ChIJsShURU-9vxQRDT8SgliSIEQ",
+    "https://www.google.com/maps/dir/?api=1&destination=36.8506439,28.2586733&travelmode=driving",
   mapsEmbedUrl:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.658922182621!2d28.2586733!3d36.8506439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bfbd4f494428b1%3A0x4420925883123f0d!2sRiva%20Veteriner%20Klini%C4%9Fi!5e0!3m2!1str!2str!4v1780085858065!5m2!1str!2str",
   ogImagePath: "/og-image.jpg",
